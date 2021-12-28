52e0fa630428993d10a19be9e95ba3a98b1817e9
Luu Thanh Dat <datlt132@gmail.com>
2021-12-29 01:22:42 +0700

    add tiki api crawler by selenium

--- shopee.py
+++ shopee.py
@@ -1,7 +1,9 @@
-import requests,json
-
+import requests
+import json
 
 response = requests.get('https://shopee.vn/api/v4/search/search_items?by=relevancy&keyword=may%20tinh&limit=60&newest'
                         '=0&order=desc&page_type=search&scenario=PAGE_GLOBAL_SEARCH&version=2')
 data = response.text
 jsondata = json.loads(data)
+
+print(jsondata)
---
+++ tiki.py
@@ -0,0 +1,24 @@
+import json
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+SCROLL_PAUSE_TIME = 0.5
+
+
+def set_browser():
+    options = Options()
+    options.add_argument("--disable-notifications")
+    browser = webdriver.Chrome(executable_path="./chromedriver_win32/chromedriver", chrome_options=options)
+    return browser
+
+
+if __name__ == '__main__':
+    browser = set_browser()
+    browser.get("https://tiki.vn/api/v2/products?limit=48&include=advertisement&aggregations=2&"
+                "trackity_id=5d52ce02-5821-bcaf-7a2a-064cdf911585&q=laptop%20asus"
+                "&fbclid=IwAR0r5P2JzQLnhNsTaJauNwkjL957M5qa9SkATCQnYiNpgOFK40EP__nOT00")
+    sleep(1)
+    element = browser.find_element_by_tag_name("body")
+    jsondata = json.loads(element.text)
+    print(jsondata)
